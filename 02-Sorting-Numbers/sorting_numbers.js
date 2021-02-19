153467f9de9f5bbe97f28d4fd387bc136c11c625
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-19 12:06:13 +0100

    second ex

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -1,6 +1,24 @@
 //insert your pseudocode below
 /*
-here
+
 */
 
 //insert your code below
+var k;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+k = [Number(window.prompt(0)), Number(window.prompt(0)), Number(window.prompt(0))];
+window.alert(k.slice().sort(listsGetSortCompare("NUMERIC", 1)));
